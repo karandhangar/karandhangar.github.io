eaad9aceb4d644e684d3db90790e242202970e9b
karandhangar <119390012+karandhangar@users.noreply.github.com>
2023-12-01 17:17:42 -0500

    Smart spoons 1st Upload
    Smart Spoons

---
+++ home.js
@@ -0,0 +1,5 @@
+// Add this script to your HTML or a separate JS file
+function toggleDrawer() {
+    var drawer = document.querySelector('.drawer-menu');
+    drawer.style.display = (drawer.style.display === 'block') ? 'none' : 'block';
+}
